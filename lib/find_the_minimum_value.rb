94e854b3421c48f10886a56f68222a11bba06390
Andres Aguilar <aaguil3@gmail.com>
2020-05-12 00:38:36 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,11 @@
 def find_min_value(array)
-  # Add your solution here
+  count = 0 
+  while count < array.length do
+    min_value = array[0]
+    if array[count] < min_value
+      min_value = array[count]
+    end
+    count += 1 
+    min_value
+  end
 end
